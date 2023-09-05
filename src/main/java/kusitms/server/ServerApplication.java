e9c60c4388f73c10c729f017f07b43308a990e14
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-09-05 23:24:52 +0900

    fix : HTTPS Swagger 수정

--- src/main/java/kusitms/server/ServerApplication.java
+++ src/main/java/kusitms/server/ServerApplication.java
@@ -1,11 +1,14 @@
 package kusitms.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://kusitms.shop",description = "Default Server URL")})
 public class ServerApplication {
 
 	public static void main(String[] args) {
