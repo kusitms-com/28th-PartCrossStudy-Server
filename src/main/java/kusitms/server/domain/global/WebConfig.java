c600712791cf10ed2e51c98b3946059ff60274bf
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-09-05 19:25:22 +0900

    fix : CORS 재설정

---
+++ src/main/java/kusitms/server/domain/global/WebConfig.java
@@ -0,0 +1,22 @@
+package kusitms.server.domain.global;
+
+import kusitms.server.domain.article.repository.ArticleRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .maxAge(3000);
+
+    }
+}
